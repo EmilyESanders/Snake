9158a3abcede171b9af0a4dfba373c9a49e7fabf
Emily Sanders <emilyelizabethsanders@gmail.com>
2023-09-11 21:21:49 -0700

    added some additional features

--- script.js
+++ script.js
@@ -18,8 +18,25 @@ var dots = [];
 
 var canvasSelected = false;
 
+var gamePaused = false;
+
+var startTime = Date.now();
+var elapsedTime = 0;
+var highScores = JSON.parse(localStorage.getItem("highScores") )|| [];
+
 canvas.addEventListener("click", function(){
     canvasSelected = true;
+});
+
+canvas.addEventListener("space", function(){
+    canvasSelected = true;
+});
+
+canvas.addEventListener("click", function(){
+    if(gameEnded){
+        resetGame();
+        gameLoop();
+    }
 });
 
 function moveSnake() {
@@ -40,17 +57,19 @@ function drawSnake() {
 }
 
 function gameLoop() {
-    if(canvasSelected){
-    moveSnake();
-    drawSnake();
-    spawnDots();
-    checkCollision();
-    if(!gameEnded){
+    if(canvasSelected && !gamePaused){
+        moveSnake();
+        drawSnake();
+        spawnDots();
+        checkCollision();
+        elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+        canvas2d.fillText("Time: " + elapsedTime, 10, 20);
+        if(!gameEnded){
+            setTimeout(gameLoop, 100);
+        }
+    } else {
         setTimeout(gameLoop, 100);
     }
-} else {
-    setTimeout(gameLoop, 100);
-}
 }
 gameLoop();
 
@@ -70,19 +89,23 @@ document.onkeydown = function(event){
             break;
         case 39:
             if (directionX !== -10){
-            
-            directionX = 10;
-            directionY = 0;
+                directionX = 10;
+                directionY = 0;
             }
             break;
         case 40:
             if(directionY !== -10){
-            directionX = 0;
-            directionY = 10;
+               directionX = 0;
+              directionY = 10;
             }
+            break;
+        case 32:
+            gamePaused = !gamePaused;
             break;
     }
 };
+
+
 
 function spawnDots() {
     if(dots.length < 10){
@@ -104,6 +127,12 @@ function checkCollision() {
             snakeY + 10 > dots[i].y){
                 snakeLength++;
                 dots.splice(i, 1);
+                var lastSegment = snakeSegments[snakeSegments.length - 1];
+                snakeSegments.push({ x: lastSegment.x, y: lastSegment.y });
+                // if(snakeLength > highScores){
+                //     highScores = snakeLength;
+                //     localStorage.setItem("highScores", highScores);
+                // }
             }
     }
     if (snakeX < -10 ||
@@ -119,11 +148,52 @@ function checkCollision() {
         }
 }
 
+
 function gameOver() {
     setTimeout(function(){
         alert("Game over!");
+        if(snakeLength > 0){
+            checkHighScores();
+            updateHighScores();
+        }
+        alert("Click the box to start a new game");
     }, 500);
     gameEnded = true;
+}
+
+function resetGame(){
+    gameEnded = false;
+    snakeSegments = [];
+    snakeLength = 1;
+    snakeX = 0;
+    snakeY = 0;
+    directionX = 10;
+    directionY = 0;
+    dots = [];
+    startTime = Date.now();
+}
+
+function checkHighScores(){
+    highScores.push(snakeLength);
+    highScores.sort(function(a, b) { return b - a});
+    if (highScores.length > 5){
+        highScores = highScores.slice(0, 5);
+    }
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+
+function updateHighScores() {
+    var highScoresElement = document.getElementById("highScoresList");
+    highScoresElement.innerHTML = "";
+    var storedHighScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    // var ol = document.createElement("ol");
+    for(var i = 0; i < storedHighScores.length; i++){
+        var scoreElement = document.createElement("li");
+
+        scoreElement.innerHTML = (i + 1) + ": " + storedHighScores[i];
+        highScoresElement.appendChild(scoreElement);
+    }
 }
 
 
